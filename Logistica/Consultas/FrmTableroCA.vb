648bc39c4e6bc2bac8de3fa3c6d951bf0b0ad935
Martin Miño <mmino@paternal.matafuegosgeorgia.com>
2019-03-15 11:19:49 -0300

    TableroCa modif varias

--- Logistica/Consultas/FrmTableroCA.vb
+++ Logistica/Consultas/FrmTableroCA.vb
@@ -6,13 +6,20 @@ Public Class FrmTableroCA
     Dim Sql As String
     Dim FechaInicio As Date
     Dim FechaFin As Date
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        FechaInicio = New Date(Calendar.SelectionRange.End.Year, Calendar.SelectionRange.End.Month, 1)
-        FechaFin = Calendar.SelectionRange.End
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalcular.Click
+
+        FechaInicio = Calendar.SelectionRange.Start
+        FechaInicio = FechaInicio.AddDays(-1 * (FechaInicio.Day - 1))
+        FechaFin = FechaInicio.AddMonths(1)
+
+        lbl.Text = "Consulta desde " & FechaInicio.ToString("dd/MM/yy") & " hasta " & FechaFin.AddDays(-1).ToString("dd/MM/yy")
+        lbl.Visible = True
+
         TxtBoxCantidadOperaciones.Text = CantidadOperaciones.ToString
         TxtBoxCantRecargasFacturadas.Text = CantidadRecargasFacturadas.ToString
         TxtBoxCant639.Text = Cantidad639Aprobadas.ToString
-        TxtBoxFacturacion.Text = Facturacion.ToString
+        TxtBoxFacturacion.Text = Facturacion.ToString("N2")
         TxtBoxClientes.Text = ClientesNuevosoRecuperados.ToString
 
     End Sub
@@ -21,17 +28,13 @@ Public Class FrmTableroCA
         Dim facturas As Integer = 0
         Dim abo As Integer = 0
 
-
-        Sql = "select to_char(sih.accdat_0, 'YYYY-MM') AS mes , sih.sivtyp_0 as tipo, count(*) as facturas "
-        Sql &= "from sinvoice sih inner join "
-        Sql &= "sinvoicev siv on (sih.num_0 = siv.num_0) "
-        Sql &= "where rep_0 in ('CA', 'CAA') and "
-        Sql &= " (sih.sivtyp_0 = 'FAC' and sihori_0 in (3, 8) "
-        Sql &= " or "
-        Sql &= "sih.sivtyp_0 = 'ABO' and sihori_0 in (6)) "
-        Sql &= " and accdat_0 >= :datini and accdat_0 <= :datfin "
-        Sql &= "group by to_char(sih.accdat_0, 'YYYY-MM'), sih.sivtyp_0 "
-        'Sql &= "order by mes, sivtyp_0 "
+        Sql = "SELECT to_char(sih.accdat_0, 'YYYY-MM') AS mes , sih.sivtyp_0 AS tipo, count(*) AS facturas "
+        Sql &= "FROM sinvoice sih INNER JOIN "
+        Sql &= "     sinvoicev siv on (sih.num_0 = siv.num_0) "
+        Sql &= "WHERE rep_0 IN ('CA', 'CAA') and "
+        Sql &= "      (sih.sivtyp_0 = 'FAC' and sihori_0 in (3, 8) or sih.sivtyp_0 = 'ABO' and sihori_0 in (6)) AND "
+        Sql &= "      accdat_0 >= :datini AND accdat_0 < :datfin "
+        Sql &= "GROUP BY to_char(sih.accdat_0, 'YYYY-MM'), sih.sivtyp_0 "
 
         da = New OracleDataAdapter(Sql, cn)
         da.SelectCommand.Parameters.Add("datini", OracleType.DateTime).Value = FechaInicio
@@ -59,7 +62,7 @@ Public Class FrmTableroCA
         Sql &= "sinvoicev siv on (sih.num_0 = siv.num_0) "
         Sql &= "inner join  sinvoiced sivd on (sih.num_0 = sivd.num_0) "
         Sql &= "where rep_0 in ('CA', 'CAA') "
-        Sql &= " and accdat_0 >= :datini and accdat_0 <= :datfin "
+        Sql &= " and accdat_0 >= :datini and accdat_0 < :datfin "
         Sql &= "and (sivd.tsicod_3 = '303' and sivd.TSICOD_4	= '521') "
         Sql &= "group by to_char(sih.accdat_0, 'YYYY-MM'), sih.sivtyp_0 "
 
@@ -83,9 +86,9 @@ Public Class FrmTableroCA
         Dim dt As New DataTable
         Dim dr As DataRow
         Sql = "select count(*) as cantidad from CONTSERV  "
-        Sql &= "where (constrdat_0 >= :datini and constrdat_0 <= :datfin) "
-        Sql &= "and (Salrep_0 = 'CA' or salrep_0 = 'CAA') "
-        Sql &= "and itmref_0 in ('553010','553015','553016','553017')"
+        Sql &= "where constrdat_0 >= :datini and constrdat_0 < :datfin AND "
+        Sql &= "      salrep_0 IN ('CA', 'CAA') AND "
+        Sql &= "      itmref_0 in ('553010','553015','553016','553017')"
 
         da = New OracleDataAdapter(Sql, cn)
         da.SelectCommand.Parameters.Add("datini", OracleType.DateTime).Value = FechaInicio
@@ -98,11 +101,11 @@ Public Class FrmTableroCA
         Dim dt As New DataTable
         Dim dr As DataRow
         Dim valor As String
-        Sql = "select sum(sih.amtnot_0 * sns_0) as Facturacion "
-        Sql &= "from sinvoice sih inner join "
-        Sql &= "sinvoicev siv on (sih.num_0 = siv.num_0) "
-        Sql &= "where rep_0 in ('CA', 'CAA') "
-        Sql &= "and (accdat_0 >= :datini and accdat_0 <= :datfin) "
+        Sql = "SELECT sum(sih.amtnot_0 * sns_0) AS Facturacion "
+        Sql &= "FROM sinvoice sih inner join "
+        Sql &= "     sinvoicev siv on (sih.num_0 = siv.num_0) "
+        Sql &= "WHERE rep_0 in ('CA', 'CAA') AND "
+        Sql &= "      accdat_0 >= :datini and accdat_0 < :datfin "
         Sql &= "AND sih.sivtyp_0 <> 'PRF' "
 
         da = New OracleDataAdapter(Sql, cn)
@@ -117,13 +120,14 @@ Public Class FrmTableroCA
     Private Function ClientesNuevosoRecuperados() As Integer
         Dim dt As New DataTable
         Dim dr As DataRow
+
         Sql = "select COUNT(*)  as cantidad "
         Sql &= "from sinvoice sih inner join "
         Sql &= "sinvoicev siv on (sih.num_0 = siv.num_0) "
         Sql &= "where rep_0 in ('CA', 'CAA') and "
-        Sql &= "sih.sivtyp_0 = 'FAC' and "
-        Sql &= "sih.accdat_0 >= :datini and sih.accdat_0 <= :datfin and "
-        Sql &= "sih.bpr_0 not in ("
+        Sql &= "      sih.sivtyp_0 = 'FAC' and "
+        Sql &= "      sih.accdat_0 >= :datini and sih.accdat_0 <= :datfin and "
+        Sql &= "      sih.bpr_0 not in ("
         Sql &= "	  	select distinct bpr_0 "
         Sql &= "        from(sinvoice) "
         Sql &= "        where accdat_0 >= :dat and accdat_0 < :datini and "
@@ -132,10 +136,11 @@ Public Class FrmTableroCA
         da = New OracleDataAdapter(Sql, cn)
         da.SelectCommand.Parameters.Add("datini", OracleType.DateTime).Value = FechaInicio
         da.SelectCommand.Parameters.Add("datfin", OracleType.DateTime).Value = FechaFin
-        da.SelectCommand.Parameters.Add("dat", OracleType.DateTime).Value = New Date(FechaInicio.Year - 2, FechaInicio.Month, FechaInicio.Day)
+        da.SelectCommand.Parameters.Add("dat", OracleType.DateTime).Value = FechaInicio.AddYears(-2)
         da.Fill(dt)
 
         dr = dt.Rows(0)
         Return CInt(dr("cantidad").ToString)
     End Function
+
 End Class
