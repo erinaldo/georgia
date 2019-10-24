d675a415e9d313223a92bb41544424034061dbe5
Martin Miño <mmino@paternal.matafuegosgeorgia.com>
2019-10-24 14:40:09 -0300

    Update frmFce.vb

--- Logistica/Ventas/frmFce.vb
+++ Logistica/Ventas/frmFce.vb
@@ -17,29 +17,33 @@ Public Class frmFce
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Dim Resp As wsfce.ConsultarMontoObligadoRecepcionReturnType
-
         Button1.Enabled = False
 
         If Cliente.CheckCuit(TextBox1.Text) Then
 
-            Label2.Text = "Consultando con AFIP..."
-            Application.DoEvents()
-            Application.DoEvents()
+            ConsultarCuit(TextBox1.Text)
 
-            Resp = fce.ConsultarMontoObligado(CLng(TextBox1.Text))
-
-            If Resp.obligado = wsfce.SiNoSimpleType.S Then
-                Label2.Text = "Factura de Crédito: SI a partir de $" & Resp.montoDesde.ToString("N2")
-            Else
-                Label2.Text = "Factura de Crédito: NO"
-            End If
         Else
             MessageBox.Show("CUIT Inválido")
             TextBox1.Focus()
         End If
 
         Button1.Enabled = True
+    End Sub
+    Public Sub ConsultarCuit(ByVal Cuit As String)
+        Dim Resp As wsfce.ConsultarMontoObligadoRecepcionReturnType
+
+        Label2.Text = "Consultando con AFIP..."
+        Application.DoEvents()
+        Application.DoEvents()
+
+        Resp = fce.ConsultarMontoObligado(CLng(TextBox1.Text))
+
+        If Resp.obligado = wsfce.SiNoSimpleType.S Then
+            Label2.Text = "Factura de Crédito: SI a partir de $" & Resp.montoDesde.ToString("N2")
+        Else
+            Label2.Text = "Factura de Crédito: NO"
+        End If
 
     End Sub
 
