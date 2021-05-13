b90b966ea22cb2e54290f8c73728bf4beeeadeee
Martin Miño <mmino@paternal.matafuegosgeorgia.com>
2021-05-13 15:33:09 -0300

    Dias en la calle

---
+++ Logistica/Contabilidad/frmDiasCalle.vb
@@ -0,0 +1,20 @@
+﻿Imports CrystalDecisions.CrystalReports.Engine
+
+Public Class frmDiasCalle
+
+    Private Sub btnConsultar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConsultar.Click
+        Dim dcalle As New DiaCalle(cn, usr)
+        dcalle.Calcular()
+
+        Dim rpt As New ReportDocument
+
+        With rpt
+            .Load(RPTX3 & "XDECALLE.rpt")
+            .SetDatabaseLogon(DB_USR, DB_PWD)
+            .SetParameterValue("USR", usr.Codigo)
+        End With
+
+        crv.ReportSource = rpt
+
+    End Sub
+End Class
