6048e9fbb6b5847172a07c000080e38340a8592a
Martin Miño <mmino@paternal.matafuegosgeorgia.com>
2021-05-13 15:33:41 -0300

    Premios Abonos

---
+++ Logistica/Informes/frmPremiosAbonos.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPremiosAbonos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer
+        Me.btnCalcular = New System.Windows.Forms.Button
+        Me.crv = New CrystalDecisions.Windows.Forms.CrystalReportViewer
+        Me.mCalDesde = New System.Windows.Forms.DateTimePicker
+        Me.mCalHasta = New System.Windows.Forms.DateTimePicker
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.mCalHasta)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.mCalDesde)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.btnCalcular)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.crv)
+        Me.SplitContainer1.Size = New System.Drawing.Size(914, 465)
+        Me.SplitContainer1.SplitterDistance = 270
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(85, 93)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 0
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'crv
+        '
+        Me.crv.ActiveViewIndex = -1
+        Me.crv.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.crv.DisplayGroupTree = False
+        Me.crv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.crv.Location = New System.Drawing.Point(0, 0)
+        Me.crv.Name = "crv"
+        Me.crv.SelectionFormula = ""
+        Me.crv.Size = New System.Drawing.Size(640, 465)
+        Me.crv.TabIndex = 0
+        Me.crv.ViewTimeSelectionFormula = ""
+        '
+        'mCalDesde
+        '
+        Me.mCalDesde.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.mCalDesde.Location = New System.Drawing.Point(61, 12)
+        Me.mCalDesde.Name = "mCalDesde"
+        Me.mCalDesde.Size = New System.Drawing.Size(112, 20)
+        Me.mCalDesde.TabIndex = 2
+        '
+        'mCalHasta
+        '
+        Me.mCalHasta.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.mCalHasta.Location = New System.Drawing.Point(61, 45)
+        Me.mCalHasta.Name = "mCalHasta"
+        Me.mCalHasta.Size = New System.Drawing.Size(112, 20)
+        Me.mCalHasta.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Desde:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Hasta:"
+        '
+        'frmPremiosAbonos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(914, 465)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "frmPremiosAbonos"
+        Me.Text = "Premios Abonos"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents btnCalcular As System.Windows.Forms.Button
+    Friend WithEvents crv As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents mCalHasta As System.Windows.Forms.DateTimePicker
+    Friend WithEvents mCalDesde As System.Windows.Forms.DateTimePicker
+End Class
---
+++ Logistica/Informes/frmPremiosAbonos.vb
@@ -0,0 +1,119 @@
+﻿Imports System.Data.OracleClient
+Imports CrystalDecisions.CrystalReports.Engine
+
+Public Class frmPremiosAbonos
+    Private da As OracleDataAdapter
+    Private dtPrincipal As New DataTable
+    Private Desde As Date
+    Private Hasta As Date
+
+    Public Sub New()
+
+        ' Llamada necesaria para el Diseñador de Windows Forms.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        Dim Sql As String
+
+        Sql = "select distinct c.fecha_0, c.ruta_0, d.cliente_0, d.suc_0, d.direccion_0, zut.chofer_0, c.acomp_0, c.acomp_1, c.acomp_2, adi.n1_0 "
+        Sql &= "from xrutac c inner join "
+        Sql &= "     xrutad d on (c.ruta_0 = d.ruta_0) inner join "
+        Sql &= "     bpcustomer bpc on (d.cliente_0 = bpc.bpcnum_0) inner join "
+        Sql &= "     interven itn on (d.vcrnum_0 = itn.num_0) inner join "
+        Sql &= "     zunitrans zut on (zut.bptnum_0 = c.transporte_0 and zut.patnum_0 = c.patente_0) left join "
+        Sql &= "     atabdiv adi on (adi.numtab_0 = 5006 and adi.code_0 = bpc.xcomplej_0) "
+        Sql &= "where itn.typ_0 in ('A1', 'C1') and "
+        Sql &= "      c.valid_0 = 1 and "
+        Sql &= "      d.estado_0 = 3 and "
+        Sql &= "      c.fecha_0 between :desde and :hasta "
+        Sql &= "order by c.fecha_0, c.ruta_0"
+        da = New OracleDataAdapter(Sql, cn)
+        da.SelectCommand.Parameters.Add("desde", OracleType.DateTime)
+        da.SelectCommand.Parameters.Add("hasta", OracleType.DateTime)
+
+    End Sub
+    Private Sub btnCalcular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalcular.Click
+        btnCalcular.Enabled = False
+
+        Desde = mCalDesde.Value
+        Hasta = mCalHasta.Value
+
+        ObtenerDatos()
+        GrabarEnTemporal()
+        MostrarReporte()
+
+        btnCalcular.Enabled = True
+    End Sub
+    Private Sub ObtenerDatos()
+        da.SelectCommand.Parameters("desde").Value = Desde
+        da.SelectCommand.Parameters("hasta").Value = Hasta
+        dtPrincipal.Clear()
+        da.Fill(dtPrincipal)
+    End Sub
+    Private Sub GrabarEnTemporal()
+        Dim dr As DataRow
+        Dim tmp As New Temporal(cn, usr, "ABONO") 'Informacion general
+        Dim tmp2 As New Temporal(cn, usr, "ABONO2") 'Resumen premio por acompañante
+
+        tmp.Abrir()
+        tmp.LimpiarTabla()
+
+        tmp2.Abrir()
+        tmp2.LimpiarTabla()
+
+        For Each dr In dtPrincipal.Rows
+            Dim CantidadAcompanantes As Integer = 1
+
+            'If dr("cliente_0").ToString = "611448" Then Stop
+
+            tmp.Nuevo()
+            tmp.Fecha(0) = CDate(dr("fecha_0"))
+            tmp.Fecha(1) = Desde
+            tmp.Fecha(2) = Hasta
+            tmp.Cadena(0) = dr("cliente_0").ToString
+            tmp.Cadena(1) = dr("suc_0").ToString
+            tmp.Cadena(2) = dr("direccion_0").ToString
+            tmp.Cadena(3) = dr("chofer_0").ToString
+            tmp.Numero(0) = CDbl(dr("ruta_0"))
+            tmp.Numero(1) = CDbl(dr("acomp_0"))
+            tmp.Numero(2) = CDbl(dr("acomp_1"))
+            tmp.Numero(3) = CDbl(dr("acomp_2"))
+            tmp.Numero(4) = CDbl(IIf(IsDBNull(dr("n1_0")), 0, dr("n1_0"))) 'complejidad
+
+            'Calculo la cantidad de acompañantes
+            For i As Integer = 0 To 2
+                If CDbl(dr("acomp_" & i.ToString)) > 0 Then
+                    CantidadAcompanantes += 1
+                End If
+            Next
+            'Si la cantidad de acompañantes es mayor a 2 el precio
+            'se divide por esa cantidad, sino no se divide
+            If CantidadAcompanantes <= 2 Then CantidadAcompanantes = 1
+
+            'Agrego los acompañantes a la tabla de resumen
+            For i = 0 To 2
+                If CDbl(dr("acomp_" & i.ToString)) > 0 Then
+                    Dim flg As Boolean = tmp2.Buscar(0, CDbl(dr("acomp_" & i.ToString)))
+                    If Not flg Then
+                        tmp2.Nuevo()
+                        tmp2.Numero(0) = CDbl(dr("acomp_" & i.ToString))
+                    End If
+                    tmp2.Numero(1) += (tmp.Numero(4) / CantidadAcompanantes)
+                End If
+            Next
+        Next
+        tmp.Grabar()
+        tmp2.Grabar()
+
+    End Sub
+    Private Sub MostrarReporte()
+        Dim rpt As New ReportDocument
+
+        rpt.Load(RPTX3 & "XPREMIOSABONOS.rpt")
+        rpt.SetParameterValue("USR", usr.Codigo)
+        rpt.SetDatabaseLogon(DB_USR, DB_PWD)
+        crv.ReportSource = rpt
+
+    End Sub
+    
+End Class
