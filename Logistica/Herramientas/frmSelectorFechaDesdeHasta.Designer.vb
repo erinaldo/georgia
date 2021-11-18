5d225ae7766b2d8f01ba2c96d534480beb27d881
Martin Miño <mmino@matafuegosgeorgia.com>
2021-11-18 12:01:58 -0300

    Selector de Fechas

---
+++ Logistica/Herramientas/frmSelectorFechaDesdeHasta.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSelectorFechaDesdeHasta
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim Label1 As System.Windows.Forms.Label
+        Dim Label2 As System.Windows.Forms.Label
+        Me.dtpDesde = New System.Windows.Forms.DateTimePicker
+        Me.dtpHasta = New System.Windows.Forms.DateTimePicker
+        Me.btnAceptar = New System.Windows.Forms.Button
+        Me.btnCancelar = New System.Windows.Forms.Button
+        Label1 = New System.Windows.Forms.Label
+        Label2 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Label1.AutoSize = True
+        Label1.Location = New System.Drawing.Point(22, 23)
+        Label1.Name = "Label1"
+        Label1.Size = New System.Drawing.Size(74, 13)
+        Label1.TabIndex = 0
+        Label1.Text = "Fecha Desde:"
+        '
+        'Label2
+        '
+        Label2.AutoSize = True
+        Label2.Location = New System.Drawing.Point(22, 59)
+        Label2.Name = "Label2"
+        Label2.Size = New System.Drawing.Size(71, 13)
+        Label2.TabIndex = 2
+        Label2.Text = "Fecha Hasta:"
+        '
+        'dtpDesde
+        '
+        Me.dtpDesde.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpDesde.Location = New System.Drawing.Point(102, 17)
+        Me.dtpDesde.Name = "dtpDesde"
+        Me.dtpDesde.Size = New System.Drawing.Size(101, 20)
+        Me.dtpDesde.TabIndex = 1
+        '
+        'dtpHasta
+        '
+        Me.dtpHasta.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtpHasta.Location = New System.Drawing.Point(102, 53)
+        Me.dtpHasta.Name = "dtpHasta"
+        Me.dtpHasta.Size = New System.Drawing.Size(101, 20)
+        Me.dtpHasta.TabIndex = 3
+        '
+        'btnAceptar
+        '
+        Me.btnAceptar.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnAceptar.Location = New System.Drawing.Point(231, 17)
+        Me.btnAceptar.Name = "btnAceptar"
+        Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAceptar.TabIndex = 4
+        Me.btnAceptar.Text = "&Aceptar"
+        Me.btnAceptar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancelar.Location = New System.Drawing.Point(231, 54)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.TabIndex = 5
+        Me.btnCancelar.Text = "&Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'frmSelectorFechaDesdeHasta
+        '
+        Me.AcceptButton = Me.btnAceptar
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancelar
+        Me.ClientSize = New System.Drawing.Size(333, 100)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnAceptar)
+        Me.Controls.Add(Label2)
+        Me.Controls.Add(Me.dtpHasta)
+        Me.Controls.Add(Label1)
+        Me.Controls.Add(Me.dtpDesde)
+        Me.Name = "frmSelectorFechaDesdeHasta"
+        Me.Text = "Selector de Fechas"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAceptar As System.Windows.Forms.Button
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
+    Private WithEvents dtpDesde As System.Windows.Forms.DateTimePicker
+    Private WithEvents dtpHasta As System.Windows.Forms.DateTimePicker
+End Class
---
+++ Logistica/Herramientas/frmSelectorFechaDesdeHasta.vb
@@ -0,0 +1,24 @@
+﻿Public Class frmSelectorFechaDesdeHasta
+    Public Property Desde() As Date
+        Get
+            Return dtpDesde.Value
+        End Get
+        Set(ByVal value As Date)
+            dtpDesde.Value = value
+        End Set
+    End Property
+    Public Property Hasta() As Date
+        Get
+            Return dtpHasta.Value
+        End Get
+        Set(ByVal value As Date)
+            dtpHasta.Value = value
+        End Set
+    End Property
+    Private Sub btnAceptar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAceptar.Click
+        Me.DialogResult = Windows.Forms.DialogResult.OK
+    End Sub
+    Private Sub btnCancelar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelar.Click
+        Me.DialogResult = Windows.Forms.DialogResult.Cancel
+    End Sub
+End Class
